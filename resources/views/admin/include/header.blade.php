51a34464f18c2ef95444317ec150d9e14bb7c3a9
SeffenTeraNinja <66901505+SeffenTeraNinja@users.noreply.github.com>
2021-01-14 11:09:59 +0200

    header

--- resources/views/admin/include/header.blade.php
+++ resources/views/admin/include/header.blade.php
@@ -1,7 +1,7 @@
 <div class="iq-sidebar">
     <div class="iq-sidebar-logo d-flex justify-content-between">
         <a href="index.html">
-        <img src="images/logo.gif" class="img-fluid" alt="">
+        <img src="{{asset('assets/plugins/vito/images/logo.gif')}}" class="img-fluid" alt="">
         <span>Vito</span>
         </a>
         <div class="iq-menu-bt-sidebar">
@@ -83,7 +83,7 @@
         <div class="iq-sidebar-logo">
             <div class="top-logo">
                 <a href="index.html" class="logo">
-                <img src="images/logo.gif" class="img-fluid" alt="">
+                <img src="{{asset('assets/plugins/vito/images/logo.gif')}}" class="img-fluid" alt="">
                 <span>vito</span>
                 </a>
             </div>
@@ -107,13 +107,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto navbar-list">
                 <li class="nav-item">
-                    <a class="search-toggle iq-waves-effect language-title" href="#"><img src="images/small/flag-01.png" alt="img-flaf" class="img-fluid mr-1" style="height: 16px; width: 16px;" /> English <i class="ri-arrow-down-s-line"></i></a>
+                    <a class="search-toggle iq-waves-effect language-title" href="#"><img src="{{asset('assets/plugins/vito/images/small/flag-01.png')}}" alt="img-flaf" class="img-fluid mr-1" style="height: 16px; width: 16px;" /> English <i class="ri-arrow-down-s-line"></i></a>
                     <div class="iq-sub-dropdown">
-                        <a class="iq-sub-card" href="#"><img src="images/small/flag-02.png" alt="img-flaf" class="img-fluid mr-2" />French</a>
-                        <a class="iq-sub-card" href="#"><img src="images/small/flag-03.png" alt="img-flaf" class="img-fluid mr-2" />Spanish</a>
-                        <a class="iq-sub-card" href="#"><img src="images/small/flag-04.png" alt="img-flaf" class="img-fluid mr-2" />Italian</a>
-                        <a class="iq-sub-card" href="#"><img src="images/small/flag-05.png" alt="img-flaf" class="img-fluid mr-2" />German</a>
-                        <a class="iq-sub-card" href="#"><img src="images/small/flag-06.png" alt="img-flaf" class="img-fluid mr-2" />Japanese</a>
+                        <a class="iq-sub-card" href="#"><img src="{{asset('assets/plugins/vito/images/small/flag-02.png')}}" alt="img-flaf" class="img-fluid mr-2" />French</a>
+                        <a class="iq-sub-card" href="#"><img src="{{asset('assets/plugins/vito/images/small/flag-03.png')}}" alt="img-flaf" class="img-fluid mr-2" />Spanish</a>
+                        <a class="iq-sub-card" href="#"><img src="{{asset('assets/plugins/vito/images/small/flag-04.png')}}" alt="img-flaf" class="img-fluid mr-2" />Italian</a>
+                        <a class="iq-sub-card" href="#"><img src="{{asset('assets/plugins/vito/images/small/flag-05.png')}}" alt="img-flaf" class="img-fluid mr-2" />German</a>
+                        <a class="iq-sub-card" href="#"><img src="{{asset('assets/plugins/vito/images/small/flag-06.png')}}" alt="img-flaf" class="img-fluid mr-2" />Japanese</a>
 
                     </div>
                 </li>
@@ -138,7 +138,7 @@
                             <a href="#" class="iq-sub-card" >
                                 <div class="media align-items-center">
                                     <div class="">
-                                        <img class="avatar-40 rounded" src="images/user/01.jpg" alt="">
+                                        <img class="avatar-40 rounded" src="{{asset('assets/plugins/vito/images/user/01.jpg')}}" alt="">
                                     </div>
                                     <div class="media-body ml-3">
                                         <h6 class="mb-0 ">Emma Watson Nik</h6>
@@ -150,7 +150,7 @@
                             <a href="#" class="iq-sub-card" >
                                 <div class="media align-items-center">
                                     <div class="">
-                                        <img class="avatar-40 rounded" src="images/user/02.jpg" alt="">
+                                        <img class="avatar-40 rounded" src="{{asset('assets/plugins/vito/images/user/02.jpg')}}" alt="">
                                     </div>
                                     <div class="media-body ml-3">
                                         <h6 class="mb-0 ">New customer is join</h6>
@@ -162,7 +162,7 @@
                             <a href="#" class="iq-sub-card" >
                                 <div class="media align-items-center">
                                     <div class="">
-                                        <img class="avatar-40 rounded" src="images/user/03.jpg" alt="">
+                                        <img class="avatar-40 rounded" src="{{asset('assets/plugins/vito/images/user/03.jpg')}}" alt="">
                                     </div>
                                     <div class="media-body ml-3">
                                         <h6 class="mb-0 ">Two customer is left</h6>
@@ -174,7 +174,7 @@
                             <a href="#" class="iq-sub-card" >
                                 <div class="media align-items-center">
                                     <div class="">
-                                        <img class="avatar-40 rounded" src="images/user/04.jpg" alt="">
+                                        <img class="avatar-40 rounded" src="{{asset('assets/plugins/vito/images/user/04.jpg')}}" alt="">
                                     </div>
                                     <div class="media-body ml-3">
                                         <h6 class="mb-0 ">New Mail from Fenny</h6>
@@ -201,7 +201,7 @@
                             <a href="#" class="iq-sub-card" >
                                 <div class="media align-items-center">
                                     <div class="">
-                                        <img class="avatar-40 rounded" src="images/user/01.jpg" alt="">
+                                        <img class="avatar-40 rounded" src="{{asset('assets/plugins/vito/images/user/01.jpg')}}" alt="">
                                     </div>
                                     <div class="media-body ml-3">
                                         <h6 class="mb-0 ">Nik Emma Watson</h6>
@@ -212,7 +212,7 @@
                             <a href="#" class="iq-sub-card" >
                                 <div class="media align-items-center">
                                     <div class="">
-                                        <img class="avatar-40 rounded" src="images/user/02.jpg" alt="">
+                                        <img class="avatar-40 rounded" src="{{asset('assets/plugins/vito/images/user/02.jpg')}}" alt="">
                                     </div>
                                     <div class="media-body ml-3">
                                         <h6 class="mb-0 ">Lorem Ipsum Watson</h6>
@@ -223,7 +223,7 @@
                             <a href="#" class="iq-sub-card" >
                                 <div class="media align-items-center">
                                     <div class="">
-                                        <img class="avatar-40 rounded" src="images/user/03.jpg" alt="">
+                                        <img class="avatar-40 rounded" src="{{asset('assets/plugins/vito/images/user/03.jpg')}}" alt="">
                                     </div>
                                     <div class="media-body ml-3">
                                         <h6 class="mb-0 ">Why do we use it?</h6>
@@ -234,7 +234,7 @@
                             <a href="#" class="iq-sub-card" >
                                 <div class="media align-items-center">
                                     <div class="">
-                                        <img class="avatar-40 rounded" src="images/user/04.jpg" alt="">
+                                        <img class="avatar-40 rounded" src="{{asset('assets/plugins/vito/images/user/04.jpg')}}" alt="">
                                     </div>
                                     <div class="media-body ml-3">
                                         <h6 class="mb-0 ">Variations Passages</h6>
@@ -245,7 +245,7 @@
                             <a href="#" class="iq-sub-card" >
                                 <div class="media align-items-center">
                                     <div class="">
-                                        <img class="avatar-40 rounded" src="images/user/05.jpg" alt="">
+                                        <img class="avatar-40 rounded" src="{{asset('assets/plugins/vito/images/user/05.jpg')}}" alt="">
                                     </div>
                                     <div class="media-body ml-3">
                                         <h6 class="mb-0 ">Lorem Ipsum generators</h6>
@@ -262,7 +262,7 @@
             <ul class="navbar-list">
                 <li>
                 <a href="#" class="search-toggle iq-waves-effect d-flex align-items-center">
-                    <img src="images/user/1.jpg" class="img-fluid rounded mr-3" alt="user">
+                    <img src="{{asset('assets/plugins/vito/images/user/1.jpg')}}" class="img-fluid rounded mr-3" alt="user">
                     <div class="caption">
                         <h6 class="mb-0 line-height">Nik jone</h6>
                         <span class="font-size-12">Available</span>
