6b5d44a22cfeb44196335c3c13876c575a817352
ahmedabdelraouf <ahmednsreldeen94@gmail.com>
2021-01-21 00:25:23 +0200

    no edits

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -2,37 +2,36 @@
 
 namespace App\Services;
 
-use App\Models\Cart;
+use App\Http\Traits\BasicTrait;
 use App\Models\Offer;
-use App\Models\Order;
 use App\Repositories\CartRepository;
 use App\Repositories\CityRepository;
 use App\Repositories\OrderRepository;
- use App\Http\Traits\BasicTrait;
- use Carbon\CarbonPeriod;
- use Carbon\Carbon;
- use Auth;
- use DB;
-use Illuminate\Database\Eloquent\Model;
+use Auth;
+use DB;
 
 class OrderService
 {
 
-   use  BasicTrait;
-    protected $model ,$order;
+    use  BasicTrait;
+
+    protected $model, $order;
 
     public function __construct()
     {
 //        $this->model = $model;
         $this->order = new OrderRepository();
     }
+
     /** get all order by type  */
-    public function index(){
+    public function index()
+    {
         return $this->order->index();
     }
 
     /** add new order to database */
-    public function createOrder($request){
+    public function createOrder($request)
+    {
         $order = $this->order->createOrder($request);
         $cart = new CartRepository();
         $cart->emptyCart();
@@ -41,23 +40,27 @@ class OrderService
     }
 
     /** show specific order  */
-    public function show($id){
-         return $this->order->show($id);
+    public function show($id)
+    {
+        return $this->order->show($id);
     }
 
     /** accept updates for order */
-    public function update($updated_data){
-        return  $this->order->update($updated_data);
+    public function update($updated_data)
+    {
+        return $this->order->update($updated_data);
     }
 
     /** block specific order */
 
-    public function blockStatus($order_id){
-        return  $this->order->blockStatus( $order_id);
+    public function blockStatus($order_id)
+    {
+        return $this->order->blockStatus($order_id);
     }
 
-    public function unblockStatus( $order_id){
-        return  $this->order->unblockStatus( $order_id);
+    public function unblockStatus($order_id)
+    {
+        return $this->order->unblockStatus($order_id);
     }
 
 //    public function getServices(){
@@ -65,61 +68,70 @@ class OrderService
 //    }
 
     /** delete order */
-    public function delete(){
+    public function delete()
+    {
         return $this->order->delete();
     }
 
     /** update status for order by accept or reject  */
-    public function updateStatus($status ,$id){
-        return $this->order->updateStatus($status ,$id);
+    public function updateStatus($status, $id)
+    {
+        return $this->order->updateStatus($status, $id);
     }
 
-    public function getOrders(){
+    public function getOrders()
+    {
         return $this->order->getOrders();
     }
 
-    public function reOrder($orderId){
+    public function reOrder($orderId)
+    {
         return $this->order->reOrder($orderId);
     }
 
-      public function orderStatus($orderId){
+    public function orderStatus($orderId)
+    {
         return $this->order->orderStatus($orderId);
     }
 
-    public function getCities(){
+    public function getCities()
+    {
         $city = new CityRepository();
         return $city->getCities();
     }
 
-    public function offerAvailability($code){
+    public function offerAvailability($code)
+    {
         $now = date('Y-m-d H:m:i');
-        $offer = Offer::where('code',$code)
-            ->whereDate('start_at','<',$now)
-            ->whereDate('end_at','>',$now)
+        $offer = Offer::where('code', $code)
+            ->whereDate('start_at', '<', $now)
+            ->whereDate('end_at', '>', $now)
             ->whereRaw('count < uses_number OR uses_number IS NULL')
-            ->where('status','Available')
-            ->orWhere('status','Reopened')
+            ->where('status', 'Available')
+            ->orWhere('status', 'Reopened')
             ->first();
-        if($offer && !$offer->user->contains(Auth::id())){
+        if ($offer && !$offer->user->contains(Auth::id())) {
             return $offer->id;
-        } else{
+        } else {
             return null;
         }
     }
 
-    public function getOffers(){
+    public function getOffers()
+    {
         $now = date('Y-m-d H:m:i');
-        $offers = Offer::whereDate('start_at','<',$now)
-            ->whereDate('end_at','>',$now)
+        $offers = Offer::whereDate('start_at', '<', $now)
+            ->whereDate('end_at', '>', $now)
             ->whereRaw('count < uses_number OR uses_number IS NULL')
-            ->where('status','Available')
-            ->orWhere('status','Reopened')
+            ->where('status', 'Available')
+            ->orWhere('status', 'Reopened')
             ->get();
         return $offers;
     }
 
-    public function checkCode($code){
-        $offer = Offer::where('code',$code)->select('discount','discount_type')->first();
+    public function checkCode($code)
+    {
+        $offer = Offer::where('code', $code)->select('discount', 'discount_type')->first();
         $cartPrice = Auth::user()->cart->total_price;
         return $this->order->getOfferedPrice($offer, $cartPrice);
     }
