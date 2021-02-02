d9ef41016438f0b9683f2009a4ea998ded47b521
SeffenTeraNinja <66901505+SeffenTeraNinja@users.noreply.github.com>
2021-02-02 15:56:35 +0200

    edits

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -14,7 +14,15 @@
             <div class="iq-card">
                 <div class="iq-card-header d-flex justify-content-between">
                     <div class="iq-header-title">
-                        <h4 class="card-title">Edit New {{$type}}</h4>
+                        <h4 class="card-title">تعديل
+                            @if($type == 'Sacrifice')
+                            بيانات الذبيحة
+                            @elseif($type == 'Bird')
+                            بيانات الطائر
+                            @else
+                            {{$type}}
+                            @endif
+                        </h4>
                     </div>
                 </div>
             </div>
@@ -29,7 +37,7 @@
                                     <img class="profile-pic img-fluid" src="{{asset('assets/plugins/vito/images/user/11.png')}}" alt="profile-pic">
                                 @endif
                                     <div class="p-image">
-                                        <a href="#" class="upload-button btn iq-bg-primary">Edit Logo</a>
+                                        <a href="#" class="upload-button btn iq-bg-primary">تعديل الشعار</a>
                                         <input class="file-upload" @if(is_null($item->logo)) required @endif form="main-form-to-add-doctor" type="file" accept="image/*" name="logo">
                                         <input type="hidden" value="{{$item->logo}}" name="logo_path">
                                     </div>
@@ -43,28 +51,44 @@
                 <div class="iq-card">
                     <div class="iq-card-header d-flex justify-content-between">
                         <div class="iq-header-title">
-                            <h4 class="card-title">New {{$type}} Information</h4>
+                            <h4 class="card-title">
+                                بيانات
+                                @if($type == 'Sacrifice')
+                                الذبيحة
+                                @elseif($type == 'Bird')
+                                الطائر
+                                @else
+                                {{$type}}
+                                @endif
+                            </h4>
                         </div>
                     </div>
                     <div class="iq-card-body">
                         <div class="new-user-info">
                                 <div class="row">
                                     <div class="form-group col-md-6">
-                                        <label for="name">Name:</label>
-                                        <input type="text" class="form-control" id="name" name="name" value="{{old('name',$item->name)}}"  min="3"  max="100" placeholder="Enter a name">
-                                        <span class="form-text text-muted">Please enter product name</span>
+                                        <label for="name">الاسم:</label>
+                                        <input type="text" class="form-control" id="name" name="name" value="{{old('name',$item->name)}}"  min="3"  max="100" placeholder="الاسم">
                                     </div>
                                     <input type="hidden" value="{{$type}}" name="type">
                                     <div class="form-group col-md-12">
-                                        <label>description *</label>
-                                        <textarea class="form-control"   name="description" placeholder="Enter a Description"
+                                        <label>الوصف *</label>
+                                        <textarea class="form-control"   name="description" placeholder="الوصف"
                                             rows="3">{{old('description',$item->description)}}</textarea>
-                                        <span class="form-text text-muted">Please enter a menu within text length range 50 and 100.</span>
                                     </div>
                                 </div>
                                 <div class="col-md-12">
-                                    <button type="submit" class="btn btn-success ml-2 pull-right">Edit New {{$type}}</button>
-                                    <button type="reset" class="btn btn-secondary pull-right">Cancel</button>
+                                    <button type="submit" class="btn btn-primary ml-2 pull-left">
+                                        تعديل
+                                        @if($type == 'Sacrifice')
+                                        الذبيحة
+                                        @elseif($type == 'Bird')
+                                        الطائر
+                                        @else
+                                        {{$type}}
+                                        @endif
+                                    </button>
+                                    <button type="reset" class="btn btn-secondary pull-left">الغاء</button>
                                     <div class="clearfix"></div>
                                 </div>
                         </div>
