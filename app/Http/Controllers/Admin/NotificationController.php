90997998ad64df2ed8d3d6d16edb22608d2ac123
SaeedTeraNinja <70259498+SaeedTeraNinja@users.noreply.github.com>
2021-04-22 11:39:30 +0200

    notification fix

--- app/Http/Controllers/Admin/NotificationController.php
+++ app/Http/Controllers/Admin/NotificationController.php
@@ -15,7 +15,7 @@ class NotificationController extends Controller
 
     public function send(NotificationRequest $request){
         $userIds = User::all()->pluck('id')->toArray();
-        $userIds = [5];
+        $userIds = ["5"];
         $this->sendNotification($userIds, $request->title, $request->body, 'General News', null);
         return redirect()->route('notifications.create');
     }
